2c393f8567ebc24e0e7b883ffa544f1f49eadb41
Ashique-Ur-Rahman <ashik.rahman713@gmail.com>
2023-11-12 00:17:48 +0600

    Project Started - Evelyn Moore

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <h2 className="font-bold border-b-2">DebugCube Solutions Pvt. Ltd.</h2>
+
+        <p className="my-4">
+          Bangladesh's leading IT Services and software solutions company
+          DebugCube Solutions Pvt. Ltd. in Khulna, Bangladesh.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
